aee48355c2a11544c6e6497c1efc265652e26917
Elle White <ellewhitedev@gmail.com>
2023-07-25 14:32:48 -0400

    working on banner

--- app.py
+++ app.py
@@ -114,7 +114,10 @@ def login():
 def logout():
     """Handle logout of user."""
 
-    # IMPLEMENT THIS
+    do_logout()
+
+    flash('Successfully logged out', 'success')
+    return redirect('/login')
 
 
 ##############################################################################
